39a77826ae8e108c86b08aec9457a4cbc51e26b6
snbn <sb.as.engineer@gmail.com>
2019-05-18 00:44:43 +0900

    fix

--- src/client/js/app.ts
+++ src/client/js/app.ts
@@ -2,6 +2,14 @@ import { GLElement, GLWrapper } from "./gl_helper";
 
 import { mat4 } from "gl-matrix";
 
+function unwrap<T>(v: T | null): T {
+    if (v == null) {
+        throw new Error('unwrap is called with null value');
+    } else {
+        return v;
+    }
+}
+
 const vshaderSkelton = `
 attribute vec4 aVertexPosition;
 attribute vec4 aVertexColor;
@@ -25,8 +33,8 @@ void main(void) {
     gl_FragColor = factor * (vColor + colOffset);
 }`;
 
-document.getElementById("vshader-source").value = vshaderSkelton;
-document.getElementById("fshader-source").value = fshaderSkelton;
+unwrap(document.getElementById("vshader-source")).setAttribute('value', vshaderSkelton);
+unwrap(document.getElementById("fshader-source")).setAttribute('value', fshaderSkelton);
 
 let squareRotation = 0.0;
 
@@ -52,8 +60,7 @@ const state = {
     renderLastUpdated: 0.0
 };
 
-const runButton = document.getElementById('run-button');
-runButton.addEventListener('click', reload);
+unwrap(document.getElementById('run-button')).addEventListener('click', reload);
 load();
 
 function reload() {
@@ -62,7 +69,11 @@ function reload() {
 }
 
 function clean() {
-    const gl = state.gl;
+    const gl: WebGLRenderingContext | null = state.gl;
+
+    if (gl == null) {
+        return;
+    }
 
     Object.values(state.glBuffers).forEach(function (b) {
         gl.deleteBuffer(b);
@@ -76,138 +87,138 @@ function clean() {
 }
 
 function load() {
-    const canvas = document.querySelector('#glcanvas');
+    const canvas = unwrap(document.querySelector('#glcanvas')) as HTMLCanvasElement;
     if (!state.gl) {
         state.gl = canvas.getContext('webgl');
     }
-    const gl = state.gl;
+    const gl: WebGLRenderingContext | null = state.gl;
 
-    if (!gl) {
+    if (gl === null) {
         alert('Unable to initialize WebGL. Your browser or machine may not support it.');
         return;
-    }
+    } else {
+        const glw = new GLWrapper(gl);
 
-    const glw = new GLWrapper(gl);
+        const vsSource = shaderSourceFromEditor("vshader-source");
+        const fsSource = shaderSourceFromEditor("fshader-source");
 
-    const vsSource = shaderSourceFromEditor("vshader-source");
-    const fsSource = shaderSourceFromEditor("fshader-source");
+        const shaderProgram = glw.initShaderProgram(vsSource, fsSource);
 
-    const shaderProgram = glw.initShaderProgram(vsSource, fsSource);
+        const programInfo = {
+            program: shaderProgram,
+            attribLocations: {
+                vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+                vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
+            },
+            uniformLocations: {
+                projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+                modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+            },
+        };
 
-    const programInfo = {
-        program: shaderProgram,
-        attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
-            vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
-        },
-        uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
-        },
-    };
+        const uni_num = gl.getProgramParameter(shaderProgram, gl.ACTIVE_UNIFORMS);
+        const uni_info = [];
+        for (let i = 0; i < uni_num; i++) {
+            const info = gl.getActiveUniform(shaderProgram, i);
+            info.location = gl.getUniformLocation(shaderProgram, info.name);
+            uni_info.push(info);
+        }
 
-    const uni_num = gl.getProgramParameter(shaderProgram, gl.ACTIVE_UNIFORMS);
-    const uni_info = [];
-    for (let i = 0; i < uni_num; i++) {
-        const info = gl.getActiveUniform(shaderProgram, i);
-        info.location = gl.getUniformLocation(shaderProgram, info.name);
-        uni_info.push(info);
-    }
+        const uniformList = document.createElement('ol');
+        uni_info.forEach(function (v) {
+            const itemElm = document.createElement('li');
+            itemElm.id = `uniform-${v.name}`;
+            uniformList.appendChild(itemElm);
 
-    const uniformList = document.createElement('ol');
-    uni_info.forEach(function (v) {
-        const itemElm = document.createElement('li');
-        itemElm.id = `uniform-${v.name}`;
-        uniformList.appendChild(itemElm);
+            itemElm.addEventListener('change', function (ev) {
+                const row = Number.parseInt(itemElm.getAttribute('data-row'));
+                const col = Number.parseInt(itemElm.getAttribute('data-col'));
+                const elmType = Number.parseInt(itemElm.getAttribute('data-elm_type'));
+                const isMatrix = row !== 1 && row === col;
+                const isFloat = elmType === GLElement.FLOAT;
 
-        itemElm.addEventListener('change', function (ev) {
-            const row = Number.parseInt(itemElm.getAttribute('data-row'));
-            const col = Number.parseInt(itemElm.getAttribute('data-col'));
-            const elmType = Number.parseInt(itemElm.getAttribute('data-elm_type'));
-            const isMatrix = row !== 1 && row === col;
-            const isFloat = elmType === GLElement.FLOAT;
+                let suffix = isFloat ? 'f' : 'i';
+                suffix += 'v';
+                suffix = col.toString() + suffix;
+                suffix = isMatrix ? 'Matrix' + suffix : suffix;
+                const funcName = 'uniform' + suffix;
 
-            let suffix = isFloat ? 'f' : 'i';
-            suffix += 'v';
-            suffix = col.toString() + suffix;
-            suffix = isMatrix ? 'Matrix' + suffix : suffix;
-            const funcName = 'uniform' + suffix;
+                const valuesElm = document.querySelector(`#${itemElm.id} > .values`);
+                const data = new Float32Array(row * col);
 
-            const valuesElm = document.querySelector(`#${itemElm.id} > .values`);
-            const data = new Float32Array(row * col);
-
-            let currRowElm = valuesElm.firstChild;
-            for (let i = 0; i < row; i++) {
-                let currValElm = currRowElm.firstChild;
-                for (let j = 0; j < col; j++) {
-                    const a = isFloat ?
-                        Number.parseFloat(currValElm.value) : Number.parseInt(currValElm.value);
-                    data[i * col + j] = a;
-                    currValElm = currValElm.nextSibling;
+                let currRowElm = valuesElm.firstChild;
+                for (let i = 0; i < row; i++) {
+                    let currValElm = currRowElm.firstChild;
+                    for (let j = 0; j < col; j++) {
+                        const a = isFloat ?
+                            Number.parseFloat(currValElm.value) : Number.parseInt(currValElm.value);
+                        data[i * col + j] = a;
+                        currValElm = currValElm.nextSibling;
+                    }
+                    currRowElm = currRowElm.nextSibling;
                 }
-                currRowElm = currRowElm.nextSibling;
+                gl[funcName](v.location, data);
+            });
+
+            const nameElm = document.createElement('span');
+            nameElm.textContent = `${v.name} :`;
+            itemElm.appendChild(nameElm);
+
+            const typeElm = document.createElement('span');
+            typeElm.textContent = glw.typeIdToTypeName(v.type);
+            itemElm.appendChild(typeElm);
+
+            const dim = glw.TypeIdToDimension(v.type);
+            if (!dim) {
+                return;
             }
-            gl[funcName](v.location, data);
+            const elmType = glw.TypeIdtoElementType(v.type);
+            itemElm.setAttribute('data-row', dim[0]);
+            itemElm.setAttribute('data-col', dim[1]);
+            itemElm.setAttribute('data-elm_type', elmType);
+
+            const valuesElm = document.createElement('div');
+            valuesElm.className = 'values';
+            itemElm.appendChild(valuesElm);
+
+            for (let i = 0; i < dim[0]; i++) {
+                const rowElm = document.createElement('div');
+                rowElm.class = 'matrix-row';
+                valuesElm.appendChild(rowElm);
+                for (let j = 0; j < dim[1]; j++) {
+                    const valueElm = document.createElement('input');
+                    valueElm.type = 'text';
+                    rowElm.appendChild(valueElm);
+                    valueElm.value = 0;
+                }
+            }
         });
+        document.getElementById('variables').appendChild(uniformList);
 
-        const nameElm = document.createElement('span');
-        nameElm.textContent = `${v.name} :`;
-        itemElm.appendChild(nameElm);
+        const buffers = glw.initBuffers();
 
-        const typeElm = document.createElement('span');
-        typeElm.textContent = glw.typeIdToTypeName(v.type);
-        itemElm.appendChild(typeElm);
+        state.canvas = canvas;
+        state.gl = gl;
+        state.shaderInfo = programInfo;
+        state.glBuffers = buffers;
 
-        const dim = glw.TypeIdToDimension(v.type);
-        if (!dim) {
-            return;
-        }
-        const elmType = glw.TypeIdtoElementType(v.type);
-        itemElm.setAttribute('data-row', dim[0]);
-        itemElm.setAttribute('data-col', dim[1]);
-        itemElm.setAttribute('data-elm_type', elmType);
+        const renderLoopId = state.renderLoopId;
+        squareRotation = 0.0;
 
-        const valuesElm = document.createElement('div');
-        valuesElm.className = 'values';
-        itemElm.appendChild(valuesElm);
-
-        for (let i = 0; i < dim[0]; i++) {
-            const rowElm = document.createElement('div');
-            rowElm.class = 'matrix-row';
-            valuesElm.appendChild(rowElm);
-            for (let j = 0; j < dim[1]; j++) {
-                const valueElm = document.createElement('input');
-                valueElm.type = 'text';
-                rowElm.appendChild(valueElm);
-                valueElm.value = 0;
+        function render(now) {
+            if (renderLoopId != state.renderLoopId) {
+                console.log(`render loop ${renderLoopId} has been finished`);
+                return;
             }
+            now *= 0.001;
+            const deltaTime = now - state.renderLastUpdated;
+            state.renderLastUpdated = now;
+
+            drawScene(gl, programInfo, buffers, deltaTime);
+            requestAnimationFrame(render);
         }
-    });
-    document.getElementById('variables').appendChild(uniformList);
-
-    const buffers = glw.initBuffers();
-
-    state.canvas = canvas;
-    state.gl = gl;
-    state.shaderInfo = programInfo;
-    state.glBuffers = buffers;
-
-    const renderLoopId = state.renderLoopId;
-    squareRotation = 0.0;
-
-    function render(now) {
-        if (renderLoopId != state.renderLoopId) {
-            console.log(`render loop ${renderLoopId} has been finished`);
-            return;
-        }
-        now *= 0.001;
-        const deltaTime = now - state.renderLastUpdated;
-        state.renderLastUpdated = now;
-
-        drawScene(gl, programInfo, buffers, deltaTime);
         requestAnimationFrame(render);
     }
-    requestAnimationFrame(render);
 }
 
 function drawScene(gl, programInfo, buffers, deltaTime) {
