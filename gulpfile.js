10a2d81b13dbf8ef6fefcef0ee61a1b3bd6a84ae
snbn <sb.as.engineer@gmail.com>
2019-05-20 21:37:21 +0900

    introduce browserify and babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
-const { spawn } = require('child_process');
-const { watch } = require('gulp');
+const fs = require('fs');
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const tsify = require('tsify');
 
-function pack(cb) {
-    var p = spawn('npx', ['webpack']);
-    p.stderr.on('data', function (data) {
-        console.log(`stderr: ${data}`);
-    });
-    p.stdout.on('data', function (data) {
-        console.log(`stdout: ${data}`);
-    });
-    p.on('close', function (code) {
-        console.log(`npx exit with code ${code}`);
-    });
-    cb();
+function bundleScript() {
+    return browserify('src/client/script/app.ts')
+        .plugin(tsify)
+        .transform(babelify)
+        .bundle().pipe(fs.createWriteStream('public/out/bundle.js'));
 }
 
-watch('src/client/**', pack);
+function copyHtml() {
+    return gulp.src('src/client/index.html').pipe(gulp.dest('public'));
+}
 
-exports.default = pack;
+function copyCss() {
+    return gulp.src('src/client/style/app.css').pipe(gulp.dest('public/css'));
+}
+
+function copyShader() {
+    return gulp.src('src/client/shader/*').pipe(gulp.dest('public/shader'));
+}
+
+const all = gulp.parallel(copyShader, copyCss, copyHtml, bundleScript);
+
+exports.default = all;
